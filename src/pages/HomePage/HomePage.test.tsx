1f60a55a3a8f7b097168708e28415347a500fe88
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-06-11 22:22:41 -0300

    feat: add health card feature

--- src/pages/HomePage/HomePage.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import { HomePage } from './'
-
-test('renders Home page heading', () => {
-  const { getByText } = render(<HomePage />)
-  const headingElement = getByText(/Home page/i)
-  expect(headingElement).toBeInTheDocument()
-})
