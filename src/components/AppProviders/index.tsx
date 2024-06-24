333697ac9341b5593d4a6aa134c1cb503391c256
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-06-24 18:47:34 -0300

    refact: refactor app providers

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from '../../styles/themes/default'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { MockUserProvider } from './contexts/MockUserContext'
 
 interface AppProvidersProps {
   children: ReactNode
@@ -13,9 +12,7 @@ const queryClient = new QueryClient()
 const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={defaultTheme}>
-        <MockUserProvider>{children}</MockUserProvider>
-      </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
     </QueryClientProvider>
   )
 }
